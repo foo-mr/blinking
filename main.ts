89c147741e3552d528852189ffdf1cace5f96c67
TY <86838308+foo-mr@users.noreply.github.com>
2022-09-25 13:49:45 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,16 @@
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.pause(200)
+    }
+})
